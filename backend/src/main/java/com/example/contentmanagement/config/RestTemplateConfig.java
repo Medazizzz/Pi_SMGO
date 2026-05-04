2ccba5856caf150f385f38097bec57138fa75e1d
Ranim Thlibi <Ranim.Thlibi@esprit.tn>
2026-05-05 01:10:46 +0200

    fix frontend missing dependencies

--- backend/src/main/java/com/example/contentmanagement/config/RestTemplateConfig.java
+++ backend/src/main/java/com/example/contentmanagement/config/RestTemplateConfig.java
@@ -1,21 +1,4 @@
-package com.example.contentmanagement;
+package com.example.contentmanagement.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
-
-@SpringBootApplication
-@EnableScheduling
-public class ContentManagementApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ContentManagementApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+public class RestTemplateConfig {
 }
